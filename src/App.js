d964df0732bfc56200346a966b605cc3caf6ea9b
Graham Hagenah <gwhagenah@gmail.com>
2023-12-03 12:27:55 -0500

    rm node_mod and implementing keydown events for synth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import * as Tone from 'tone'
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
 
-  const synth = new Tone.Synth().toDestination()
+  const synth =  new Tone.PolySynth(Tone.Synth).toDestination()
   const [octave, setOctave] = useState(4);
 
   // Capture state variables
   let currentOctave = octave
 
+  useEffect(() => {
+    // Focus on App after render to allow immediate acess for handleKeyDown
+    document.getElementsByTagName("main")[0].focus()
+  }, [])
 
   const Piano = ({synth, octave}) => {
     const whiteKeys = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
@@ -31,26 +35,48 @@ function App() {
     )
   }
 
-  const PianoKey = (props) => {
-    const playNote = async () => {
-      await Tone.start()
-      props.synth.triggerAttackRelease((props.note + props.octave), '8n');
-    }
+  const playNote = async (note, octave, synth) => {
+    await Tone.start()
+    synth.triggerAttackRelease((note + octave), '8n')
+  }
 
+  const PianoKey = (props) => {
     return (
-      <button onClick={playNote}>
-          {props.note}
+      <button onClick={() => playNote(props.note, props.octave, props.synth)}>
+        {props.note}
       </button>
     ) 
   }
+
+  const handleKeyDown = (props) => {
+    const note = noteMapping[props.key];
+    if (note) {
+      playNote(note, currentOctave, synth)
+    }
+  }
+
+  const noteMapping = {
+    'a': 'C',
+    'w': 'C#',
+    's': 'D',
+    'e': 'D#',
+    'd': 'E',
+    'f': 'F',
+    't': 'F#',
+    'g': 'G',
+    'y': 'G#',
+    'h': 'A',
+    'u': 'A#',
+    'j': 'B'
+  }
   
   return (
-    <div className="App">
+    <main className="App" tabIndex="0" onKeyDown={handleKeyDown}>
       <header className="App-header">
         <Piano synth={synth} octave={currentOctave} />
         <button onClick={() => setOctave(5)}>Set Octave</button>
       </header>
-    </div>
+    </main>
   )
 }
 
